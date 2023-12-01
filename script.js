a0f62ffa82228c47aa861c613ea178ce6170fc52
Mayara Amorim <mayara.moreira@fenixeditorial.com.br>
2023-12-01 14:44:17 -0300

    Tratando erro com bloco  try catch

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const containerVideos = document.querySelector(".videos__container")
 
 async function buscar() {
-    const busca = await fetch("http://localhost:3000/videos");
-   const videos = await busca.json();// .then(res=>res.json())//precisamos transformar em json pois esse fetch vai devolver uma promise
-                                    // .then((videos)=>
-    videos.forEach(video=> {
-       containerVideos.innerHTML+=`
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();// .then(res=>res.json())//precisamos transformar em json pois esse fetch vai devolver uma promise
+    // .then((videos)=>
+    videos.forEach(video => {
+        containerVideos.innerHTML += `
         <li class="videos__item">
         <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
         <div class="descricao-video">
@@ -16,6 +17,10 @@ async function buscar() {
         </li>
        `;
     })
+    }catch(e){
+       containerVideos.innerHTML = `O correu o erro: ${e}. Por isso não foi possivel exibir os conteudos!`
+    }
+    
 }
 
 buscar();
